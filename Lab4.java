1aee8937be4558c80031444fd7618ca67d639f17
Cassie Schultz <CassieSchultz35@gmail.com>
2020-09-10 14:00:23 -0500

    Updated version of code

--- Lab4.java
+++ Lab4.java
@@ -1,3 +1,5 @@
+// used this source for reverse sorting array: https://www.java67.com/2016/07/how-to-sort-array-in-descending-order-in-java.html
+
 import java.util.*;
 public class Lab4{
     public static void main(String[] args){
@@ -12,23 +14,54 @@ public class Lab4{
         }
         int binCapacity = parameters[0];
         int n = parameters[1];
-        int[] itemArray = new int[parameters.length-2];
+        Integer[] itemArray = new Integer[parameters.length-2];
         for(int i =0; i < n; i++){
         	itemArray[i] = parameters[i+2];
         } 
-
+        System.out.println();
+       
         // Test prints to make sure input is being taken correctly
-        System.out.println(binCapacity);
-        System.out.println(n);
+
+
         for(int i=0; i< itemArray.length; i++){
         	System.out.println(itemArray[i]);
         }
-        Collections.reverse(Arrays.sort(itemArray));
+        
 
     }//end main
 
     public static void packBin(int[] itemArray, int binCapacity){
-        //Collections.reverse(Arrays.sort(itemArray));
+       // sorting the items in reverse order
+       ArrayList<Integer> bins = new ArrayList<Integer>();
+       ArrayList<Integer> binsOutput = new ArrayList<Integer>();
+       Arrays.sort(itemArray, Collections.reverseOrder());
+       for(int i = 0; i < itemArray.length; i++){
+           bins.add(binCapacity);
+       }
+       for(int i=0; i< itemArray.length; i++){
+            boolean itemPlaced = false; 
+            // continue until item has been placed
+            int binCount = 0;
+            while(!itemPlaced){
+                if (itemArray[i] <= bin.get(binCount)){
+                    bin.get(binCount) = bin.get(binCount) - itemArray[i];
+                    System.out.println("item " + itemArray[i] + " placed in bin " + binCount);
+                    itemPlaced= true;
+
+                }
+                else if(itemArray[i] > bin.get(binCount)){
+                    binCount++;
+                   
+                }
+
+
+            }sss
+
+       }
+      
+
+
+
 
 
     } 
